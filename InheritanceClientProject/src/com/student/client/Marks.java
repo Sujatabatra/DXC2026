d34b3d20ae7dba3049eda0eef2e29ee82e0deb55
Sujatabatra <sujatabatra@hotmail.com>
2026-06-03 19:32:16 +0530

    Add files via upload

---
+++ InheritanceClientProject/src/com/student/client/Marks.java
@@ -0,0 +1,46 @@
+package com.student.client;
+
+import com.sujata.demo.Student;
+
+/*
+ * Advantage of Inheritance :
+ * 1. Resuability
+ * 2. Extensibility
+ */
+//Sub class in different Package
+public class Marks extends Student {
+
+	private int marks1;
+	private int marks2;
+	private int marks3;
+	
+	
+	public void input(int rollNumber,String name,int marks1,int marks2, int marks3) {
+		input(rollNumber, name);
+		this.marks1=marks1;
+		this.marks2=marks2;
+		this.marks3=marks3;
+	}
+	
+	public void show() {
+		display();
+		
+		System.out.println("Marks1 : "+marks1);
+		System.out.println("Marks2 : "+marks2);
+		System.out.println("Marks3 : "+marks3);
+	}
+
+	public int getMarks1() {
+		return marks1;
+	}
+
+	public int getMarks2() {
+		return marks2;
+	}
+
+	public int getMarks3() {
+		return marks3;
+	}
+	
+	
+}
---
+++ InheritanceClientProject/src/com/student/client/StudentSport.java
@@ -0,0 +1,32 @@
+package com.student.client;
+
+import com.sujata.demo.Student;
+
+public class StudentSport extends Student {
+
+	private String sportName;
+	private int sportScore;
+	
+	public void input(int rollNumber,String name,String sportName,int sportScore) {
+		input(rollNumber, name);
+		this.sportName=sportName;
+		this.sportScore=sportScore;
+	}
+	
+	public void show() {
+		display();
+		System.out.println("Sport Name : "+sportName);
+		System.out.println("Sport Score : "+sportScore);
+	}
+
+	public String getSportName() {
+		return sportName;
+	}
+
+	public int getSportScore() {
+		return sportScore;
+	}
+	
+	
+	
+}
